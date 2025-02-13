b3e4f74bb4fb944291577d27974bdc73fe94ec08
Anna Karslyan <ankarslyan@yandex.ru>
2025-02-13 19:48:05 +0400

    gcd

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # hexlet-braingame
 
 brain-even cast: https://asciinema.org/a/R9coOPfqySczMjADQ1eChVYUe
+
+brain-calc cast: https://asciinema.org/a/ysNUc3W4d3WHzNwShp6oGt0jB
+
+brain-gcd cast: https://asciinema.org/a/FRQKGVVGXswGy8ZwVe1Jqzdrz
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,47 @@
+import random
+import prompt
+
+def gcd(a, b):
+    r = a % b
+    while r != 0:
+        a = b
+        b = r
+        r = a % b
+    return b
+
+def ask(name):
+    a = random.randint(1,100)
+    b = random.randint(1,100)
+    nod = gcd(a, b)
+    exp = f'{a} {b}'
+    print(f'Question: {exp}')
+    answer = prompt.string('Your answer: ')
+    try:
+        if answer == str(nod):
+            print('Correct!')
+            return True
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{nod}'.\n Let's try again, Sam!")
+            return False
+        
+    except:
+        print(f"'{answer}' is wrong answer ;(. Correct answer was '{nod}'.\n Let's try again, Sam!")
+        return False
+    
+def main():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Find the greatest common divisor of given numbers.')
+    counter = 0
+    while True: 
+        if counter == 3:
+            print(f'Congratulations, {name}!')
+            break
+        if ask(name):
+            counter += 1
+        else:
+            counter = 0
+
+if __name__ == "__main__":
+    main()
