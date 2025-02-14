1bc1bf6fddd2f63553a362dca360b89b8effbe75
Anna Karslyan <ankarslyan@yandex.ru>
2025-02-14 20:50:30 +0400

    prime

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ brain-calc cast: https://asciinema.org/a/ysNUc3W4d3WHzNwShp6oGt0jB
 brain-gcd cast: https://asciinema.org/a/FRQKGVVGXswGy8ZwVe1Jqzdrz
 
 brain-progression: https://asciinema.org/a/wvu2Q94xbPacIpwCxChYiZznh
+
+brain-prime: https://asciinema.org/a/ukoY91d7apLkVrrOYfrBU01mY
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,38 @@
+import random
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def main():
+    print("Welcome to the Brain Games!")
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+
+    correct_answers_count = 0  
+
+    while True:
+        number = random.randint(1, 100) 
+        correct_answer = "yes" if is_prime(number) else "no"
+        
+        print(f"Question: {number}")
+        user_answer = input('Your answer: ').strip().lower()
+        
+        if user_answer == correct_answer:
+            correct_answers_count += 1
+            print("Correct!")
+            if correct_answers_count == 3:
+                print(f'Congratulations, {name}!')
+                correct_answers_count = 0  
+                break
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.\n Let's try again, {name}!")
+            correct_answers_count = 0  
+            
+if __name__ == "__main__":
+    main()
